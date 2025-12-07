4648c7c362ddf2c1dc9d67dd49d809b55e663131
Rich M <rmery32@hotmail.com>
2025-12-07 18:30:48 +0000

    added missing ;

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Image from "next/image";
-import { navItems } from "@/data"
+import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="relative bg-black-100 overflow-hidden mx-auto flex flex-col items-center justify-center sm:px-10 px-5">
