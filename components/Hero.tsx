5eecb9ce80dad3ac1f9610fc1a29c1446d9471d5
Rich M <rmery32@hotmail.com>
2025-12-07 15:37:41 +0000

    amends to grid bg in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,8 +5,7 @@ const Hero = () => {
     return (
         <div className="pb-20 pt-36">
             <Spotlight />
-            <Spotlight />
-            <div className="flex h-screen w-full items-center justify-center opacity-30">
+            <div className="absolute top-0 left-0 h-screen flex h-[50rem] w-full items-center justify-center bg-black-100[0.2]">
                 <div
                     className={cn(
                         "absolute inset-0",
@@ -16,7 +15,12 @@ const Hero = () => {
                     )}
                 />
                 {/* Radial gradient for the container to give a faded look */}
-                {/* <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div> */}
+                <div className="pointer-events-none absolute inset-0 flex items-center justify-center  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] bg-black-100"></div>
+            </div>
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                    <h2 className="text-white">My folio</h2>
+                </div>
             </div>
         </div>
     )
